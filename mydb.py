6c7942e8b7d06db3a32e714e6fb817a27a966653
pahancha <pahanchathuranga123@gmail.com>
2023-12-10 12:14:09 +0530

    initial commit - successful mysql connect.

---
+++ mydb.py
@@ -0,0 +1,13 @@
+import mysql.connector
+
+dataBase = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    passwd = 'rootuser'
+)
+
+cursorObject = dataBase.cursor()
+
+cursorObject.execute("CREATE DATABASE crm")
+
+print("Database setup done.")
